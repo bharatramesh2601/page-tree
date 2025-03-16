e0245768d5d5c543c4ca720bfb1b1db624abce91
Bharat <bharatramesh2601@gmail.com>
2025-03-15 23:05:54 -0400

    updates

--- src/app/components/nodes.tsx
+++ src/app/components/nodes.tsx
@@ -1,7 +1,19 @@
-const Nodes = ({}) => {
-    return (
-        <div>Nodes</div>
-    )
+const Node = ({ label, link }) => {
+  return (
+    <li>
+        <a href={link}>{label}</a>;
+    </li>
+  )
+};
+
+const Nodes = ({ nodes }) => {
+  return (
+    <ul className="nodes">
+      {nodes.map((node) => {
+        return <Node key={node.id} {...node} />;
+      })}
+    </ul>
+  );
 };
 
 export default Nodes;
